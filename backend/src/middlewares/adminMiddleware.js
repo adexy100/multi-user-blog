9989efb17d4a80a5aed28bb4768c6b4e20029e7f
unknown <adexy100@gmail.com>
2021-08-18 12:49:01 -1200

    middleware init

---
+++ backend/src/middlewares/adminMiddleware.js
@@ -0,0 +1,19 @@
+exports.adminMiddleware = (req, res, next) => {
+    const adminUserId = req.user._id;
+    User.findById({ _id: adminUserId }).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            });
+        }
+
+        if (user.role !== 1) {
+            return res.status(400).json({
+                error: 'Admin resource. Access denied'
+            });
+        }
+
+        req.profile = user;
+        next();
+    });
+};
---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,12 @@
+export default authMiddleware = (req, res, next) => {
+	const authUserId = req.user._id;
+	User.findById({ _id: authUserId }). exec((err, user) => {
+		if (err || !user) {
+			return res,status(400).json({
+				error: 'Access denied, Please Login'
+			});
+		}
+		req.profile = user;
+		next();
+	});
+};
